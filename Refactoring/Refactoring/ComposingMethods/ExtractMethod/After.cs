9d1e1b4e90f12b3d6954a6a3a1df7025ab403d52
estcab <estcab@gmail.com>
2014-07-14 18:44:00 +0200

    Extract Method example

---
+++ Refactoring/Refactoring/ComposingMethods/ExtractMethod/After.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring.ComposingMethods.ExtractMethod
+{
+    public class After
+    {
+        private readonly IQueryable<Order> _orders;
+        private readonly string _name;
+
+        public After(IQueryable<Order> orders, string name)
+        {
+            _orders = orders;
+            _name = name;
+        }
+
+        public void PrintOwing()
+        {
+            PrintBanner();
+            PrintDetails(GetOutstanding());
+        }
+
+        private double GetOutstanding()
+        {
+            IList<Order> orders = _orders.ToList();
+            return orders.Sum(order => order.Amount);
+        }
+
+        private void PrintDetails(double outstanding)
+        {
+            Console.WriteLine("name:" + _name);
+            Console.WriteLine("amount" + outstanding);
+        }
+
+        private void PrintBanner()
+        {
+            Console.WriteLine("**************************");
+            Console.WriteLine("***** Customer Owes ******");
+            Console.WriteLine("**************************");
+        }
+    }
+}
---
+++ Refactoring/Refactoring/ComposingMethods/ExtractMethod/Before.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactoring.ComposingMethods.ExtractMethod
+{
+    public class Before
+    {
+        private readonly IQueryable<Order> _orders;
+        private readonly string _name;
+
+        public Before(IQueryable<Order> orders, string name)
+        {
+            _orders = orders;
+            _name = name;
+        }
+
+        public void PrintOwing()
+        {
+            IList<Order> orders = _orders.ToList();
+
+            double outstanding = 0.0;
+
+            // print banner
+            Console.WriteLine("**************************");
+            Console.WriteLine("***** Customer Owes ******");
+            Console.WriteLine("**************************");
+
+            // calculate outstanding
+            foreach (var order in orders)
+            {
+                outstanding += order.Amount;
+            }
+
+
+            //print details
+            Console.WriteLine("name:" + _name);
+            Console.WriteLine("amount" + outstanding);
+
+        }
+    }
+}
