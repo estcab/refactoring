c0478635a7799867bd22883926cbd59bd1ea9d96
estcab <estcab@gmail.com>
2014-07-15 06:31:45 +0200

    inline method

---
+++ Refactoring/Refactoring/ComposingMethods/InlineMethod/After.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring.ComposingMethods.InlineMethod
+{
+    public class After
+    {
+        private readonly decimal _numberOfLateDeliveries;
+
+        public After(decimal numberOfLateDeliveries)
+        {
+            _numberOfLateDeliveries = numberOfLateDeliveries;
+        }
+
+        public int GetRating()
+        {
+            return _numberOfLateDeliveries > 5 ? 2 : 1;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/ComposingMethods/InlineMethod/Before.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring.ComposingMethods.InlineMethod
+{
+    public class Before
+    {
+        private readonly decimal _numberOfLateDeliveries;
+
+        public Before(decimal numberOfLateDeliveries)
+        {
+            _numberOfLateDeliveries = numberOfLateDeliveries;
+        }
+
+        public int GetRating()
+        {
+            return (MoreThanFiveLateDeliveries()) ? 2 : 1;
+        }
+
+        private bool MoreThanFiveLateDeliveries()
+        {
+            return _numberOfLateDeliveries > 5;
+        }
+
+    }
+}
