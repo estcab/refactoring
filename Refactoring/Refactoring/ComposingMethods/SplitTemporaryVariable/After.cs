e767d23c9664eb64dbe829200a778f0b41a59652
estcab <estcab@gmail.com>
2014-07-15 18:44:15 +0200

    Split Temporary Variable

---
+++ Refactoring/Refactoring/ComposingMethods/SplitTemporaryVariable/After.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Refactoring.ComposingMethods.SplitTemporaryVariable
+{
+    public class After
+    {
+        private readonly double _primaryForce;
+        private readonly double _secondaryForce;
+        private readonly double _mass;
+        private readonly int _delay;
+
+        public After(double primaryForce, double mass, int delay, double secondaryForce)
+        {
+            _primaryForce = primaryForce;
+            _mass = mass;
+            _delay = delay;
+            _secondaryForce = secondaryForce;
+        }
+
+        double GetDistanceTravelled(int time)
+        {
+            double result;
+
+            double primaryAcc = _primaryForce / _mass;
+            
+            int primaryTime = Math.Min(time, _delay);
+            result = 0.5 * primaryAcc * primaryTime * primaryTime;
+            int secondaryTime = time - _delay;
+            if (secondaryTime > 0)
+            {
+                double primaryVel = primaryAcc * _delay;
+                
+                double secondaryAcc = (_primaryForce + _secondaryForce) / _mass;
+                result += primaryVel * secondaryTime + 0.5 * secondaryAcc * secondaryTime * secondaryTime;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/ComposingMethods/SplitTemporaryVariable/Before.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Refactoring.ComposingMethods.SplitTemporaryVariable
+{
+    public class Before
+    {
+        private readonly double _primaryForce;
+        private readonly double _secondaryForce;
+        private readonly double _mass;
+        private readonly int _delay;
+        
+        public Before(double primaryForce, double mass, int delay, double secondaryForce)
+        {
+            _primaryForce = primaryForce;
+            _mass = mass;
+            _delay = delay;
+            _secondaryForce = secondaryForce;
+        }
+
+        double GetDistanceTravelled(int time)
+        {
+            double result;
+
+            // a temporary variable assigned to more than once
+            double acc = _primaryForce / _mass;
+            int primaryTime = Math.Min(time, _delay);
+            result = 0.5 * acc * primaryTime * primaryTime;
+            int secondaryTime = time - _delay;
+            if (secondaryTime > 0)
+            {
+                double primaryVel = acc * _delay;
+                // a temporary variable assigned to more than once
+                acc = (_primaryForce + _secondaryForce) / _mass;
+                result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+            }
+
+            return result;
+        }
+    }
+}
