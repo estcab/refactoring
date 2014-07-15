75fb9942879675e53f86c77fc020d7094b49eb40
estcab <estcab@gmail.com>
2014-07-15 07:58:38 +0200

    Replace Temp with Query

---
+++ Refactoring/Refactoring/ComposingMethods/ReplaceTempWithQuery/After.cs
@@ -0,0 +1,29 @@
+﻿namespace Refactoring.ComposingMethods.ReplaceTempWithQuery
+{
+    public class After
+    {
+        private readonly decimal _quantity;
+        private readonly decimal _itemPrice;
+
+        public After(decimal itemPrice, decimal quantity)
+        {
+            _itemPrice = itemPrice;
+            _quantity = quantity;
+        }
+
+        public decimal GetPrice()
+        {
+            return GetBasePrice() * GetDiscountFactor();
+        }
+
+        private decimal GetDiscountFactor()
+        {
+            return GetBasePrice() > 1000 ? 0.95M : 0.98M;
+        }
+
+        private decimal GetBasePrice()
+        {
+            return _quantity * _itemPrice;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/ComposingMethods/ReplaceTempWithQuery/Before.cs
@@ -0,0 +1,32 @@
+﻿namespace Refactoring.ComposingMethods.ReplaceTempWithQuery
+{
+    public class Before
+    {
+        private readonly decimal _quantity;
+        private readonly decimal _itemPrice;
+
+        public Before(decimal itemPrice, decimal quantity)
+        {
+            _itemPrice = itemPrice;
+            _quantity = quantity;
+        }
+
+        public decimal GetPrice()
+        {
+            decimal basePrice = _quantity * _itemPrice;
+            decimal discountFactor;
+
+            if (basePrice > 1000)
+            {
+                discountFactor = 0.95M;
+            }            
+            else
+            {
+                discountFactor = 0.98M;
+            }
+
+            return basePrice * discountFactor;
+        }
+
+    }
+}
