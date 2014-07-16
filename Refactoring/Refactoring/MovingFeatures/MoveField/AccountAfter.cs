8c42838981b1cb6ab5e14626effd700651fad600
estcab <estcab@gmail.com>
2014-07-16 19:07:58 +0200

    Move Field

---
+++ Refactoring/Refactoring/MovingFeatures/MoveField/AccountAfter.cs
@@ -0,0 +1,22 @@
+﻿namespace Refactoring.MovingFeatures.MoveField
+{
+    public class AccountAfter
+    {
+        private readonly AccountTypeAfter _type;
+
+        public AccountAfter(AccountTypeAfter type, double interestRate)
+        {
+            _type = type;
+        }
+
+        public double InterestForAmountDays(double amount, int days)
+        {
+            return _type.InterestRate * amount * days / 365;
+        }
+    }
+
+    public class AccountTypeAfter
+    {
+        public double InterestRate { get; set; }
+    }
+}
---
+++ Refactoring/Refactoring/MovingFeatures/MoveField/AccountBefore.cs
@@ -0,0 +1,24 @@
+﻿namespace Refactoring.MovingFeatures.MoveField
+{
+    public class AccountBefore
+    {
+        private AccountTypeBefore _type;
+        private readonly double _interestRate;
+
+        public AccountBefore(AccountTypeBefore type, double interestRate)
+        {
+            _type = type;
+            _interestRate = interestRate;
+        }
+
+        public double InterestForAmountDays(double amount, int days)
+        {
+            return _interestRate * amount * days / 365;
+        }
+
+    }
+
+    public class AccountTypeBefore
+    {
+    }
+}
