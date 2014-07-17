6a24df65a698215f0ebb9af890c603a0e05846e9
estcab <estcab@gmail.com>
2014-07-17 17:38:01 +0200

    Remove Middle Man

---
+++ Refactoring/Refactoring/MovingFeatures/RemoveMiddleMan/After/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Refactoring.MovingFeatures.RemoveMiddleMan.After
+{
+    public class Person
+    {
+        private Department _department;
+        
+        public Person(Department department)
+        {
+            SetDepartment(department);
+        }
+
+        private void SetDepartment(Department value)
+        {
+            _department = value;
+        }
+
+        public Department GetDepartment()
+        {
+            return _department;
+        }
+
+
+    }
+}
