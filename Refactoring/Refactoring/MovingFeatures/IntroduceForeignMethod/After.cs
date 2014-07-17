980960354009589d957609fa7e057c1de4b57674
estcab <estcab@gmail.com>
2014-07-17 18:39:21 +0200

    Introduce Foreign Method

---
+++ Refactoring/Refactoring/MovingFeatures/IntroduceForeignMethod/After.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Refactoring.MovingFeatures.IntroduceForeignMethod
+{
+    public class After
+    {
+        public void Example(DateTime previousEnd)
+        {
+            // ...
+
+            var newStart = GetNextDay(previousEnd);
+            
+            // ...
+        }
+
+        
+        private static DateTime GetNextDay(DateTime dateTime)
+        {
+            // foreign method, should be on date
+            return new DateTime(dateTime.Year, 
+                dateTime.Month, dateTime.Day + 1);
+        }
+    }
+}
