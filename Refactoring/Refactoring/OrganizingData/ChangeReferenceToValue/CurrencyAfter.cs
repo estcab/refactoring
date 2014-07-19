acd0f1ef7a5a8740f0c3b2d956c7e4beae11ff43
estcab <estcab@gmail.com>
2014-07-19 07:17:09 +0200

    Change Reference to Value

---
+++ Refactoring/Refactoring/OrganizingData/ChangeReferenceToValue/CurrencyAfter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Refactoring.OrganizingData.ChangeReferenceToValue
+{
+    public class CurrencyAfter
+    {
+        private readonly string _code;
+
+        private CurrencyAfter(string code)
+        {
+            _code = code;
+        }
+
+        public string GetCode
+        {
+            get { return _code; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CurrencyAfter))
+            {
+                return false;
+            }
+
+            var other = (CurrencyAfter)obj;
+
+            return (_code.Equals(other._code));
+        }
+
+        public override int GetHashCode()
+        {
+            return _code.GetHashCode();
+        }
+    }
+}
---
+++ Refactoring/Refactoring/OrganizingData/ChangeReferenceToValue/CurrencyBefore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Refactoring.OrganizingData.ChangeReferenceToValue
+{
+    public class CurrencyBefore
+    {
+        private readonly string _code;
+
+        private static readonly Dictionary<string, CurrencyBefore> Currencies = new Dictionary<string, CurrencyBefore>()
+        {
+            {"USD", new CurrencyBefore("USD")}, {"EU", new CurrencyBefore("EU")}
+        };
+
+        private CurrencyBefore(string code)
+        {
+            _code = code;
+        }
+
+        public string GetCode
+        {
+            get { return _code; }
+        }
+
+        public static CurrencyBefore GetCurrency(string code)
+        {
+            return Currencies[code];
+        }
+    }
+}
