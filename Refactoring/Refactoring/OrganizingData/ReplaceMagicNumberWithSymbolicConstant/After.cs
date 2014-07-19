83bca6f3d7bcdc711cea33dd2ae70979af9ca8df
estcab <estcab@gmail.com>
2014-07-19 09:25:48 +0200

    Replace Magic Number with Symbolic Constant

---
+++ Refactoring/Refactoring/OrganizingData/ReplaceMagicNumberWithSymbolicConstant/After.cs
@@ -0,0 +1,12 @@
+﻿namespace Refactoring.OrganizingData.ReplaceMagicNumberWithSymbolicConstant
+{
+    public class After
+    {
+        public const double GravitationalConstant = 9.81;
+
+        public double PotentialEnergy(double mass, double height)
+        {
+            return mass * GravitationalConstant * height;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/OrganizingData/ReplaceMagicNumberWithSymbolicConstant/Before.cs
@@ -0,0 +1,11 @@
+﻿namespace Refactoring.OrganizingData.ReplaceMagicNumberWithSymbolicConstant
+{
+    public class Before
+    {
+        public double PotentialEnergy(double mass, double height)
+        {
+            return mass * 9.81 * height;
+        }
+
+    }
+}
