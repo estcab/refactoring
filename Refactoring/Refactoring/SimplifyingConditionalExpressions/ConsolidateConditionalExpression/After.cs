8d2aa37786723117c87c26945e2fc8c6183f68eb
estcab <estcab@gmail.com>
2014-07-21 17:57:05 +0200

    Consolidate Conditional Expression

---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/ConsolidateConditionalExpression/After.cs
@@ -0,0 +1,45 @@
+﻿namespace Refactoring.SimplifyingConditionalExpressions.ConsolidateConditionalExpression
+{
+    public class After
+    {
+        private readonly decimal _seniority;
+        private readonly decimal _monthsDisabled;
+        private readonly bool _isPartTime;
+
+        public After(decimal seniority, decimal monthsDisabled, bool isPartTime)
+        {
+            _seniority = seniority;
+            _monthsDisabled = monthsDisabled;
+            _isPartTime = isPartTime;
+        }
+
+        public double DisabilityAmount()
+        {
+            if (IsNotEligibleForDisability()) return 0;
+
+            // compute the disability amount
+            // ...
+            return 1;
+        }
+
+        private bool IsNotEligibleForDisability()
+        {
+            return (_seniority < 2) || (_monthsDisabled > 12) || (_isPartTime);
+        }
+
+        public double OtherExample()
+        {
+            return OnVacation() && (LengthOfService() > 10) ? 1 : 0.5;
+        }
+
+        private double LengthOfService()
+        {
+            return 3;
+        }
+
+        private bool OnVacation()
+        {
+            return true;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/ConsolidateConditionalExpression/Before.cs
@@ -0,0 +1,45 @@
+﻿namespace Refactoring.SimplifyingConditionalExpressions.ConsolidateConditionalExpression
+{
+    public class Before
+    {
+        private readonly decimal _seniority;
+        private readonly decimal _monthsDisabled;
+        private readonly bool _isPartTime;
+
+        public Before(decimal seniority, decimal monthsDisabled, bool isPartTime)
+        {
+            _seniority = seniority;
+            _monthsDisabled = monthsDisabled;
+            _isPartTime = isPartTime;
+        }
+
+        public double DisabilityAmount()
+        {
+            if (_seniority < 2) return 0;
+            if (_monthsDisabled > 12) return 0;
+            if (_isPartTime) return 0;
+
+            // compute the disability amount
+            // ...
+            return 1;
+        }
+
+        public double OtherExample()
+        {
+            if (OnVacation())
+                if (LengthOfService() > 10)
+                    return 1;
+            return 0.5;
+        }
+
+        private double LengthOfService()
+        {
+            return 3;
+        }
+
+        private bool OnVacation()
+        {
+            return true;
+        }
+    }
+}
