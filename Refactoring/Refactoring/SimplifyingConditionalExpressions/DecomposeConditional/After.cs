92b1a62fa0d9056f85ff2bca02a479738f03833e
estcab <estcab@gmail.com>
2014-07-21 17:42:17 +0200

    Decompose Conditional

---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/DecomposeConditional/After.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Refactoring.SimplifyingConditionalExpressions.DecomposeConditional
+{
+    public class After
+    {
+        public static readonly string SummerStart = "2014/06/01";
+        public static readonly string SummerEnd = "2014/09/22";
+
+        private readonly decimal _winterRate;
+        private readonly decimal _summerRate;
+        private readonly decimal _winterServiceCharge;
+
+        public After(decimal winterRate, decimal summerRate, decimal winterServiceCharge)
+        {
+            _winterRate = winterRate;
+            _summerRate = summerRate;
+            _winterServiceCharge = winterServiceCharge;
+        }
+
+        public decimal GetCharge(DateTime date, decimal quantity)
+        {
+            return NotSummer(date) ? WinterCharge(quantity) : SummerCharge(quantity);
+        }
+
+        private decimal SummerCharge(decimal quantity)
+        {
+            return quantity * _summerRate;
+        }
+
+        private decimal WinterCharge(decimal quantity)
+        {
+            return quantity * _winterRate + _winterServiceCharge;
+        }
+
+        private bool NotSummer(DateTime date)
+        {
+            return date.CompareTo(DateTime.Parse(SummerStart)) < 0
+                   || date.CompareTo(DateTime.Parse(SummerEnd)) > 0;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/DecomposeConditional/Before.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Refactoring.SimplifyingConditionalExpressions.DecomposeConditional
+{
+    public class Before
+    {
+        public static readonly string SummerStart = "2014/06/01";
+        public static readonly string SummerEnd = "2014/09/22";
+
+        private readonly decimal _winterRate;
+        private readonly decimal _summerRate;
+        private readonly decimal _winterServiceCharge;
+
+        public Before(decimal winterRate, decimal summerRate, decimal winterServiceCharge)
+        {
+            _winterRate = winterRate;
+            _summerRate = summerRate;
+            _winterServiceCharge = winterServiceCharge;
+        }
+
+        public decimal GetCharge(DateTime date, decimal quantity)
+        {
+            decimal charge;
+
+            if (date.CompareTo(DateTime.Parse(SummerStart)) < 0 
+                || date.CompareTo(DateTime.Parse(SummerEnd)) > 0)
+            {
+                charge = quantity * _winterRate + _winterServiceCharge;
+            }
+            else
+            {
+                charge = quantity * _summerRate;
+            }
+
+            return charge;
+        }
+    }
+}
