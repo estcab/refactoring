8183fa9681f78dd95783de7d8add4e4c7a920a32
estcab <estcab@gmail.com>
2014-07-21 18:14:09 +0200

    Consolidate Duplicate Conditional Fragments

---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragments/After.cs
@@ -0,0 +1,36 @@
+﻿namespace Refactoring.SimplifyingConditionalExpressions.ConsolidateDuplicateConditionalFragments
+{
+    public class After
+    {
+        private const double SpecialDiscount = 0.95;
+        private const double Discount = 0.98;
+
+        public double Example(double price)
+        {
+            var total = IsSpecialDeal() ? ApplySpecialDiscount(price) : ApplyDiscount(price);
+
+            Send();
+            return total;
+        }
+
+        private double ApplyDiscount(double price)
+        {
+            return price * Discount;
+        }
+
+        private double ApplySpecialDiscount(double price)
+        {
+            return price * SpecialDiscount;
+        }
+
+        private void Send()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool IsSpecialDeal()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragments/Before.cs
@@ -0,0 +1,33 @@
+﻿namespace Refactoring.SimplifyingConditionalExpressions.ConsolidateDuplicateConditionalFragments
+{
+    public class Before
+    {
+        public double Example(double price)
+        {
+            double total;
+
+            if (IsSpecialDeal())
+            {
+                total = price * 0.95;
+                Send();
+            }
+            else
+            {
+                total = price * 0.98;
+                Send();
+            }
+
+            return total;
+        }
+
+        private void Send()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool IsSpecialDeal()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
