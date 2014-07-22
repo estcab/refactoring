d64bfd326986b0f213c88ef4fa28fd4ea52b697b
estcab <estcab@gmail.com>
2014-07-22 17:50:16 +0200

    Replace Nested Conditional with Guard Clauses

---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/ReplaceNestedConditionalWithGuardClauses/After.cs
@@ -0,0 +1,63 @@
+﻿namespace Refactoring.SimplifyingConditionalExpressions.ReplaceNestedConditionalWithGuardClauses
+{
+    public class After
+    {
+        private readonly bool _isDead;
+        private readonly bool _isSeparated;
+        private readonly bool _isRetired;
+        private readonly double _capital;
+        private readonly double _duration;
+        private readonly double _intRate;
+        private readonly double _income;
+        private const double AdjFactor = 0.92;
+
+        public After(bool isDead, bool isSeparated, bool isRetired, double capital, double duration, double intRate, double income)
+        {
+            _isDead = isDead;
+            _isSeparated = isSeparated;
+            _isRetired = isRetired;
+            _capital = capital;
+            _duration = duration;
+            _intRate = intRate;
+            _income = income;
+        }
+
+        public double GetPayAmount()
+        {
+            if (_isDead) return DeadAmount();
+            if (_isSeparated) return SeparatedAmount();
+            if (_isRetired) return RetiredAmount();
+
+            return NormalPayAmount();
+        }
+
+        public double GetAdjustedCapital()
+        {
+            if (_capital <= 0.0) return 0.0;
+            if (_intRate <= 0.0 || _duration <= 0.0) return 0.0;
+            
+            return (_income / _duration) * AdjFactor;
+        }
+
+        private double NormalPayAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double RetiredAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double SeparatedAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double DeadAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
---
+++ Refactoring/Refactoring/SimplifyingConditionalExpressions/ReplaceNestedConditionalWithGuardClauses/Before.cs
@@ -0,0 +1,77 @@
+﻿namespace Refactoring.SimplifyingConditionalExpressions.ReplaceNestedConditionalWithGuardClauses
+{
+    public class Before
+    {
+        private readonly bool _isDead;
+        private readonly bool _isSeparated;
+        private readonly bool _isRetired;
+        private readonly double _capital;
+        private readonly double _duration;
+        private readonly double _intRate;
+        private readonly double _income;
+        private const double AdjFactor = 0.92;
+
+        public Before(bool isDead, bool isSeparated, bool isRetired, double capital, double duration, double intRate, double income)
+        {
+            _isDead = isDead;
+            _isSeparated = isSeparated;
+            _isRetired = isRetired;
+            _capital = capital;
+            _duration = duration;
+            _intRate = intRate;
+            _income = income;
+        }
+
+        public double GetPayAmount()
+        {
+            double result;
+            if (_isDead) result = DeadAmount();
+            else
+            {
+                if (_isSeparated) result = SeparatedAmount();
+                else
+                {
+                    if (_isRetired) result = RetiredAmount();
+                    else result = NormalPayAmount();
+                };
+            }
+            return result;
+        }
+
+
+        public double GetAdjustedCapital()
+        {
+            double result = 0.0;
+            if (_capital > 0.0)
+            {
+                if (_intRate > 0.0 && _duration > 0.0)
+                {
+                    result = (_income / _duration) * AdjFactor;
+                }
+            }
+            return result;
+        }
+
+
+        private double NormalPayAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double RetiredAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double SeparatedAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private double DeadAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
