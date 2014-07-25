64f55a0ec2ca31f795d24458b7ac5f030f28acca
estcab <estcab@gmail.com>
2014-07-25 18:40:21 +0200

    Separate Query from Modifier

---
+++ Refactoring/Refactoring/MakingMethodCallsSimpler/SeparateQueryFromModifier/After.cs
@@ -0,0 +1,47 @@
+﻿namespace Refactoring.MakingMethodCallsSimpler.SeparateQueryFromModifier
+{
+    public class After
+    {
+        public void SendAlert(string[] people)
+        {
+            if (!FoundPerson(people).Equals(string.Empty))
+            {
+                SendAlert();
+            }
+        }
+
+        public string FoundPerson(string[] people)
+        {
+            foreach (string person in people)
+            {
+                if (person.Equals("Don"))
+                {
+                    return "Don";
+                }
+                if (person.Equals("John"))
+                {
+                    return "John";
+                }
+            }
+            return "";
+        }
+
+        public void CheckSecurity(string[] people)
+        {
+            SendAlert(people);
+            var found = FoundPerson(people);
+            SomeLaterCode(found);
+        }
+
+
+        private void SomeLaterCode(string found)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SendAlert()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Refactoring/Refactoring/MakingMethodCallsSimpler/SeparateQueryFromModifier/Before.cs
@@ -0,0 +1,40 @@
+﻿namespace Refactoring.MakingMethodCallsSimpler.SeparateQueryFromModifier
+{
+    public class Before
+    {
+        public string FoundMiscreant(string[] people)
+        {
+            foreach (string person in people)
+            {
+                if (person.Equals("Don"))
+                {
+                    sendAlert();
+                    return "Don";
+                }
+                if (person.Equals("John"))
+                {
+                    sendAlert();
+                    return "John";
+                }
+            }
+            return "";
+        }
+
+        public void CheckSecurity(string[] people)
+        {
+            string found = FoundMiscreant(people);
+            SomeLaterCode(found);
+        }
+
+
+        private void SomeLaterCode(string found)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void sendAlert()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
