23634499e40c2cf8feaf826df88442a40e263d6b
estcab <estcab@gmail.com>
2014-07-25 17:22:10 +0200

    Rename Method

---
+++ Refactoring/Refactoring/MakingMethodCallsSimpler/RenameMethod/After.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Refactoring.MakingMethodCallsSimpler.RenameMethod
+{
+    public class After
+    {
+        private readonly string _officeAreaCode;
+        private readonly string _officeNumber;
+
+        public After(string officeAreaCode, string officeNumber)
+        {
+            _officeAreaCode = officeAreaCode;
+            _officeNumber = officeNumber;
+        }
+
+        [Obsolete("Use GetOfficeTelephoneNumber instead")]
+        public string GetTelephoneNumber()
+        {
+            return GetOfficeTelephoneNumber();
+        }
+
+        public string GetOfficeTelephoneNumber()
+        {
+            return string.Format("({0}) {1}", _officeAreaCode, _officeNumber);
+        }
+    }
+}
---
+++ Refactoring/Refactoring/MakingMethodCallsSimpler/RenameMethod/Before.cs
@@ -0,0 +1,19 @@
+﻿namespace Refactoring.MakingMethodCallsSimpler.RenameMethod
+{
+    public class Before
+    {
+        private readonly string _officeAreaCode;
+        private readonly string _officeNumber;
+
+        public Before(string officeAreaCode, string officeNumber)
+        {
+            _officeAreaCode = officeAreaCode;
+            _officeNumber = officeNumber;
+        }
+
+        public string GetTelephoneNumber()
+        {
+            return string.Format("({0}) {1}", _officeAreaCode, _officeNumber);
+        }
+    }
+}
