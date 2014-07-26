ad1efa66b050ce569943f0101e66c6a28897a91d
estcab <estcab@gmail.com>
2014-07-26 17:51:36 +0200

    Replace Parameter with Method

---
+++ Refactoring/Refactoring/MakingMethodCallsSimpler/ReplaceParameterWithMethod/After.cs
@@ -0,0 +1,29 @@
+﻿namespace Refactoring.MakingMethodCallsSimpler.ReplaceParameterWithMethod
+{
+    public class After
+    {
+        private readonly decimal _quantity;
+        private readonly decimal _itemPrice;
+
+        public After(decimal quantity, decimal itemPrice)
+        {
+            _quantity = quantity;
+            _itemPrice = itemPrice;
+        }
+
+        public decimal GetPrice()
+        {
+            return DiscountLevel == 2 ? BasePrice * 0.1M : BasePrice * 0.05M;
+        }
+
+        private decimal BasePrice
+        {
+            get { return _quantity * _itemPrice; }
+        }
+
+        private int DiscountLevel
+        {
+            get { return _quantity > 100 ? 2 : 1; }
+        }
+    }
+}
---
+++ Refactoring/Refactoring/MakingMethodCallsSimpler/ReplaceParameterWithMethod/Before.cs
@@ -0,0 +1,46 @@
+﻿namespace Refactoring.MakingMethodCallsSimpler.ReplaceParameterWithMethod
+{
+    public class Before
+    {
+        private readonly decimal _quantity;
+        private readonly decimal _itemPrice;
+
+        public Before(decimal quantity, decimal itemPrice)
+        {
+            _quantity = quantity;
+            _itemPrice = itemPrice;
+        }
+
+        public decimal GetPrice()
+        {
+            var basePrice = _quantity * _itemPrice;
+
+            int discountLevel;
+
+            if (_quantity > 100)
+            {
+                discountLevel = 2;
+            }
+            else
+            {
+                discountLevel = 1;
+            }
+
+            var finalPrice = discountedPrice(basePrice, discountLevel);
+            return finalPrice;
+        }
+
+        private decimal discountedPrice(decimal basePrice, decimal discountLevel)
+        {
+            if (discountLevel == 2)
+            {
+                return basePrice * 0.1M;
+            }
+            else
+            {
+                return basePrice * 0.05M;
+            }
+
+        }
+    }
+}
