d6dc73452f490a4f425c95f542958170752be6fb
estcab <estcab@gmail.com>
2014-07-29 18:09:54 +0200

    Pull Up Method

---
+++ Refactoring/Refactoring/DealingWithGeneralization/PullUpMethod/After.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace Refactoring.DealingWithGeneralization.PullUpMethod.After
+{
+    public abstract class Customer
+    {
+        protected DateTime LastBillDate;
+
+        public void AddBill(DateTime date, double charge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateBill(DateTime date)
+        {
+            double chargeAmount = ChargeFor(LastBillDate, date);
+            AddBill(date, chargeAmount);
+        }
+
+        public abstract double ChargeFor(DateTime lastBillDate, DateTime date);
+    }
+
+    public class PreferredCustomer : Customer
+    {
+        public override double ChargeFor(DateTime lastBillDate, DateTime date)
+        {
+            // custom implementation
+
+            return 1.0;
+        }
+    }
+
+    public class RegularCustomer : Customer
+    {
+        public override double ChargeFor(DateTime lastBillDate, DateTime date)
+        {
+            // custom implementation
+
+            return 1.0;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/DealingWithGeneralization/PullUpMethod/Before.cs
@@ -0,0 +1,44 @@
+﻿using System;
+// ReSharper disable once CheckNamespace
+
+
+namespace Refactoring.DealingWithGeneralization.PullUpMethod.Before
+{
+    public class Customer
+    {
+        protected DateTime LastBillDate;
+
+        public void AddBill(DateTime date, double charge)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PreferredCustomer : Customer
+    {
+        public void CreateBill(DateTime date)
+        {
+            double chargeAmount = ChargeFor(LastBillDate, date);
+            AddBill(date, chargeAmount);
+        }
+
+        public double ChargeFor(DateTime lastBillDate, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RegularCustomer : Customer
+    {
+        public void CreateBill(DateTime date)
+        {
+            double chargeAmount = ChargeFor(LastBillDate, date);
+            AddBill(date, chargeAmount);
+        }
+
+        public double ChargeFor(DateTime lastBillDate, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
