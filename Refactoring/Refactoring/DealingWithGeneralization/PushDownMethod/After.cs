e66f25831bf86bf84dbeddd0d672e9de2f7bc7e7
estcab <estcab@gmail.com>
2014-07-29 18:23:40 +0200

    Push Down Method

---
+++ Refactoring/Refactoring/DealingWithGeneralization/PushDownMethod/After.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Refactoring.DealingWithGeneralization.PushDownMethod.After
+{
+    class Employee
+    {
+    }
+
+    class Manager : Employee
+    {
+    }
+
+    class Salesman : Employee
+    {
+        public double GetCuota()
+        {
+            return 100.0;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/DealingWithGeneralization/PushDownMethod/Before.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Refactoring.DealingWithGeneralization.PushDownMethod.Before
+{
+    class Employee
+    {
+        public double GetCuota()
+        {
+            return 100.0;
+        }
+    }
+
+    class Manager : Employee
+    {
+    }
+
+    class Salesman : Employee
+    {
+    }
+}
