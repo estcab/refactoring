2ceef5ad75f1f5b92433da9e008ec479a800f181
estcab <estcab@gmail.com>
2014-07-29 17:31:13 +0200

    Pull Up Constructor Body

---
+++ Refactoring/Refactoring/DealingWithGeneralization/PullUpConstructorBody/After.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Refactoring.DealingWithGeneralization.PullUpConstructorBody.After
+{
+
+    public class Employee
+    {
+        protected string Name;
+        protected string Id;
+
+        public Employee(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        private int _grade;
+
+        public Manager(string name, string id, int grade)
+            : base(name, id)
+        {
+            _grade = grade;
+        }
+    }
+}
---
+++ Refactoring/Refactoring/DealingWithGeneralization/PullUpConstructorBody/Before.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Refactoring.DealingWithGeneralization.PullUpConstructorBody.Before
+{
+
+    public class Employee
+    {
+        protected string _name;
+        protected string _id;
+    }
+
+    public class Manager : Employee
+    {
+        private int _grade;
+
+        public Manager(string name, string id, int grade)
+        {
+            _name = name;
+            _id = id;
+            _grade = grade;
+        }
+
+    }
+}
