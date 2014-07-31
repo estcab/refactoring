91b457f406504a04e773152d9e580f7ee69aab50
estcab <estcab@gmail.com>
2014-07-31 18:33:21 +0200

    Replace Delegation with Inheritance

---
+++ Refactoring/Refactoring/DealingWithGeneralization/ReplaceDelegationWithInheritance/After.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Refactoring.DealingWithGeneralization.ReplaceDelegationWithInheritance.After
+{
+    public class Employee : Person
+    {       
+        public override string ToString()
+        {
+            return "Emp: " + GetLastName();
+        }
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string GetLastName()
+        {
+            return Name.Substring(Name.LastIndexOf(' ') + 1);
+        }
+    }
+}
---
+++ Refactoring/Refactoring/DealingWithGeneralization/ReplaceDelegationWithInheritance/Before.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Refactoring.DealingWithGeneralization.ReplaceDelegationWithInheritance.Before
+{
+    class Employee
+    {
+        private readonly Person _person = new Person();
+
+        public string Name
+        {
+            get { return _person.Name; }
+            set { _person.Name = value; }
+        }
+        
+        public override string ToString()
+        {
+            return "Emp: " + _person.GetLastName();
+        }
+    }
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string GetLastName()
+        {
+            return Name.Substring(Name.LastIndexOf(' ') + 1);
+        }
+    }
+}
