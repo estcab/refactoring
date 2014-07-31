90fb906eae5917f34aafd33d7ebb389f1947c6e6
estcab <estcab@gmail.com>
2014-07-31 18:25:44 +0200

    Replace Inheritance with Delegation

---
+++ Refactoring/Refactoring/DealingWithGeneralization/ReplaceInheritanceWithDelegation/After.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+// ReSharper disable once CheckNamespace
+
+
+namespace Refactoring.DealingWithGeneralization.ReplaceInheritanceWithDelegation.After
+{
+    public class MyStack
+    {
+        private readonly ArrayList _array;
+
+        public MyStack()
+        {
+            this._array = new ArrayList();
+        }
+
+        public void Push(Object element)
+        {
+            _array.Insert(0, element);
+        }
+
+        public Object Pop()
+        {
+            var result = _array[0];
+            _array.RemoveAt(0);
+            return result;
+        }
+
+        public int Size()
+        {
+            return _array.Count;
+        }
+
+        public bool IsEmpty()
+        {
+            return _array.Count == 0;
+        }
+    }
+
+}
---
+++ Refactoring/Refactoring/DealingWithGeneralization/ReplaceInheritanceWithDelegation/Before.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+// ReSharper disable once CheckNamespace
+
+
+namespace Refactoring.DealingWithGeneralization.ReplaceInheritanceWithDelegation.Before
+{
+    public class MyStack : ArrayList
+    {
+        public void Push(Object element)
+        {
+            Insert(0, element);
+        }
+
+        public Object Pop()
+        {
+            var result = this[0];
+            RemoveAt(0);
+            return result;
+        }
+    }
+
+}
